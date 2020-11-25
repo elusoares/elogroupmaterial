e90f706cec45fb1c974ff07961547ca7552e3910
Elusai Soares <elusaisoares@gmail.com>
2020-11-25 10:30:04 -0300

    style root toolbar and newlead

--- src/app/modules/root/root.module.ts
+++ src/app/modules/root/root.module.ts
@@ -5,12 +5,18 @@ import { RootComponent } from './root.component';
 import { RouterModule } from '@angular/router';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [RootComponent],
   imports: [
     CommonModule,
     RouterModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
     MatToolbarModule
   ]
 })
